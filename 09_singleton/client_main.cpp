d4d6f59b006d68a6e78d57e21da252fc22ba4d37
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-02-27 07:55:46 +0000

    Draft of a Singleton thread safe.

--- 09_singleton/client_main.cpp
+++ 09_singleton/client_main.cpp
@@ -1,15 +1,35 @@
+#include <thread>
+
 #include "09_singleton/singleton.hpp"
 
 
+void zero_function();
+void ten_function();
+
 int main()
 {
+    std::thread first(zero_function);
+    std::thread second(ten_function);
+
+    first.join();
+    second.join();
+
     Singleton* myPtr = Singleton::get_istance();
 
+    // Value should be 12.
     myPtr->print_counter();
 
-    Singleton* myPtr2 = Singleton::get_istance();
-
-    myPtr2->print_counter();
-
     return 0;
+}
+
+void zero_function()
+{
+    std::sleep(1000);
+
+    Singleton* myPtr = Singleton::get_instance(0);
+}
+
+void ten_function()
+{
+    Singleton* myPtr = Singleton::get_instance(10);
 }
