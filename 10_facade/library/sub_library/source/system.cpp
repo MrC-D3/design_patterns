f04c953a327993a1451febbd74e681ac7f1c64d4
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-02-28 17:00:38 +0000

    First draft of the Facade pattern.

---
+++ 10_facade/library/sub_library/source/system.cpp
@@ -0,0 +1,12 @@
+#include "10_facade/system.hpp"
+
+
+void Class1::do_something()
+{
+    std::cout << "I'm Class1. I'm doing something." << std::endl;
+}
+
+void Class2:do_something_else()
+{
+    std::cout << "I'm Class2. I'm doing something else." << std::endl;
+}
