2c675cea2f73a04d8dd2c2ae5546d5adaf07e912
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-03-04 15:54:55 +0000

    Added simple main.

--- 11_iterator/README.md
+++ 11_iterator/README.md
@@ -1,4 +1,6 @@
 Remember:
     - how to populate a Config.cmake.in file;
     - to distinguish between the two _INTERFACE;
-    - how to use configure_package_config_file().
+    - how to use configure_package_config_file();
+    - how install(TARGETS) works;
+    - differnces between CMAKE_ and PROJECT_ variables.
---
+++ 11_iterator/client_main.cpp
@@ -0,0 +1,37 @@
+#include "11_iterator/aggregate.hpp"
+#include "11_iterator/iterator.hpp"
+
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+    {
+        Aggregate<int> myAggregate;
+        myAggregate.addItem(0);
+        myAggregate.addItem(10);
+
+        auto iterator = myAggregate.createIterator();
+        while( !(iterator->isDone()) )
+        {
+            std::cout << "Value: " << iterator->currentItem() << std::endl;
+            iterator->next();
+        }
+    }
+
+    {
+        Aggregate<std::string> myAggregate;
+        myAggregate.addItem("CIAO");
+        myAggregate.addItem("mondo");
+
+        auto iterator = myAggregate.createIterator();
+        while( !(iterator->isDone()) )
+        {
+            std::cout << "Value: " << iterator->currentItem() << std::endl;
+            iterator->next();
+        }
+    }    
+
+    return 0;
+}
