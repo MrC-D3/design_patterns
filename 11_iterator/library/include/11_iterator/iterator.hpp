bcab53b6a5787cb7082408e9ce33b7d0952eb3ba
Marco <marco.filippi15@gmail.com>
2025-03-04 07:19:59 +0000

    Move implementations in the header file as template code requires.

--- 11_iterator/library/include/11_iterator/iterator.hpp
+++ 11_iterator/library/include/11_iterator/iterator.hpp
@@ -8,19 +8,53 @@ template <typename Item>
 class Aggregate;
 
 template <typename Item>
-class Iterator : public IteratorInterface
+class Iterator : public IteratorInterface<Item>
 {
   public:
-    Iterator(Aggregate<Item>* aggregate);
+    Iterator(Aggregate<Item>* aggregate = nullptr);
 
     void first() override;
     void next() override;
     bool isDone() override;
-    Item* currentItem() override;
+    Item currentItem() override;
 
   private:
     Aggregate<Item>* m_aggregate;
     int m_index;
 };
+
+
+
+template <typename Item>
+Iterator<Item>::Iterator(Aggregate<Item>* aggregate)
+  : m_aggregate(aggregate),
+    m_index(0)
+{
+    
+}
+
+template <typename Item>
+void Iterator<Item>::first()
+{
+    m_index = 0;
+}
+
+template <typename Item>
+void Iterator<Item>::next()
+{
+    m_index += 1;
+}
+
+template <typename Item>
+bool Iterator<Item>::isDone()
+{
+    return (m_index >= m_aggregate->size());
+}
+
+template <typename Item>
+Item Iterator<Item>::currentItem()
+{
+    return m_aggregate->getItem(m_index);
+}
 
 #endif
--- 11_iterator/library/include/11_iterator/iterator_interface.hpp
+++ 11_iterator/library/include/11_iterator/iterator_interface.hpp
@@ -2,13 +2,14 @@
 #define ITERATOR_INTERFACE_HPP
 
 
+template <typename Item>
 class IteratorInterface
 {
   public:
     virtual void first() = 0;
     virtual void next() = 0;
     virtual bool isDone() = 0;
-    virtual void currentItem() = 0;
+    virtual Item currentItem() = 0;
 };
 
 #endif
