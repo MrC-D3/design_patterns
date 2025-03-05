1302ef917ff230c783ffdad27684f4b91e60df38
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-03-05 11:59:13 +0000

    Modernize the code of the Adapter pattern.

--- 00_adapter/adaptee_dog.hpp
+++ 00_adapter/adaptee_dog.hpp
@@ -1,3 +1,4 @@
+// Usage of pragma once is compiler-specific, not a C++ standard.
 #ifndef ADAPTEE_DOG_HPP
 #define ADAPTEE_DOG_HPP
 
@@ -6,7 +7,7 @@
 class Dog_NonCompliant
 {
   public:
-    void tell()
+    void tell() const
     {
         std::cout << "I'm a Dog, non-compliant with the Target interface." <<
           std::endl;
