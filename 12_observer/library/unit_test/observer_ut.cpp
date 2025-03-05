e7127dad6bb520fdf71d1aef24227069afef13ad
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-03-05 09:11:27 +0000

    Unit testing.

--- 12_observer/library/unit_test/observer_ut.cpp
+++ 12_observer/library/unit_test/observer_ut.cpp
@@ -1,1 +1,15 @@
-#include <gtest>
+#include "12_observer/observer.hpp"
+#include "12_observer/subject.hpp"
+
+#include <gtest/gtest.h>
+
+
+TEST(ObserverTestSuite, SubjectSetState)
+{
+    Subject mySubject;
+    Observer myObserver(&mySubject);
+
+    mySubject.setState(1);
+
+    ASSERT_EQ(mySubject.getState(), 1);
+}
