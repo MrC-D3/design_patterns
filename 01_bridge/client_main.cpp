bbf12a69474975ad82734e928f0eac8ac46052fe
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-03-05 14:23:11 +0000

    Make the Bridge pattern more cpp modern.

--- 01_bridge/client_main.cpp
+++ 01_bridge/client_main.cpp
@@ -1,10 +1,14 @@
 #include "abstract_hierarchy.hpp"
 #include "implementation_hierarchy.hpp"
 
+#include <memory>
+
+
 int main()
 {
-    ColorInterface* color1 = new Color1();
-    ShapeInterface* triangle = new Triangle(color1);
+    std::unique_ptr<ColorInterface> color1 = std::make_unique<Color1>();
+    std::unique_ptr<ShapeInterface> triangle = 
+      std::make_unique<Triangle>(std::move(color1));
 
     triangle->draw();
 }
