bfac053de9a8900558b40c5d7e939c7eec78494b
MrC-D3 <marco.filippi15@gmail.com>
2025-03-06 11:45:51 +0000

    Make Factory Method pattern more compliant to modern cpp.

--- 04_factory_method/source/creator.cpp
+++ 04_factory_method/source/creator.cpp
@@ -2,13 +2,15 @@
 
 #include "04_factory_method/creator.hpp"
 
+namespace Factory
+{
 
 void Creator::do_the_logic()
 {
     std::cout << "I'm the Creator. I'll use the Product to do the logic." <<
         std::endl;
 
-    if(m_product == nullptr)
+    if(!m_product)
     {
         m_product = createProduct();
     }
@@ -19,3 +21,5 @@ void Creator::do_the_logic()
 
     m_product->operationX();
 }
+
+} // namespace Factory
