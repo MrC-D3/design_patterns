749dd33fddf6c5a5bd311ddbeabfb7c33cf30323
MrC-D3 <marco.filippi15@gmail.com>
2025-03-06 15:34:09 +0000

    Make Flyweight pattern more compliant to modern cpp.

--- 05_flyweight/client_main.cpp
+++ 05_flyweight/client_main.cpp
@@ -1,27 +1,36 @@
+#include "05_flyweight/flyweight_factory.hpp"
+
 #include <array>
 #include <string>
-
-#include "05_flyweight/flyweight_factory.hpp"
+#include <iostream>
+#include <cstdint>
 
 
 int main()
 {
+    using namespace Flyweight;
+    constexpr std::uint64_t array_size = 5U;
+    constexpr std::uint8_t ascii_m = 77U;
+    constexpr std::uint8_t ascii_a = 65U;
+
     FlyweightFactory myFactory;
-    std::array<FlyweightUnshared*, 5> myString;
+    std::array<std::unique_ptr<FlyweightUnshared>, array_size> myString;
 
     // Decimal code for M: 77.
-    myString[0] = myFactory.create_flyweight(77, 0);
-    myString[1] = myFactory.create_flyweight(77, 2);
-    myString[2] = myFactory.create_flyweight(77, 3);
+    myString[0] = myFactory.create_flyweight(ascii_m, 0U);
+    myString[1] = myFactory.create_flyweight(ascii_m, 2U);
+    myString[2] = myFactory.create_flyweight(ascii_m, 3U);
     // Decimal code for A: 65.
-    myString[3] = myFactory.create_flyweight(65, 1);
-    myString[4] = myFactory.create_flyweight(65, 4);
+    myString[3] = myFactory.create_flyweight(ascii_a, 1U);
+    myString[4] = myFactory.create_flyweight(ascii_a, 4U);
 
-    std::string canvas;
-    canvas.resize(5);
-    for(uint8_t i = 0; i < 5u; i++)
+    // Declare and initialize the std::string directly.
+    std::string canvas(array_size, ' ');
+
+    // Modern C++ loop.
+    for(const auto& flyweight : myString)
     {
-        myString[i]->draw(canvas);
+        flyweight->draw(canvas);
     }
 
     std::cout << "Result: " << canvas << std::endl;
--- 05_flyweight/include/05_flyweight/flyweight_factory.hpp
+++ 05_flyweight/include/05_flyweight/flyweight_factory.hpp
@@ -1,22 +1,41 @@
+// Usually, the behavior of the original object remains in the flyweight class.
+// In this case, whoever calls a flyweight’s method must also pass appropriate
+// bits of the extrinsic state into the method’s parameters. On the other hand,
+// the behavior can be moved to the context class, which would use the linked
+// flyweight merely as a data object.
+
 #ifndef FLYWEIGHT_FACTORY_HPP
 #define FLYWEIGHT_FACTORY_HPP
-
-#include <map>
-#include <vector>
 
 #include "05_flyweight/flyweight_shared.hpp"
 #include "05_flyweight/flyweight_unshared.hpp"
 
+#include <map>
+#include <memory>
+#include <cstdint>
+
+
+namespace Flyweight
+{
 
 class FlyweightFactory
 {
   public:
-    FlyweightUnshared* create_flyweight(uint8_t code, uint64_t position);
+    std::unique_ptr<FlyweightUnshared> create_flyweight(
+      const std::uint8_t& code, 
+      const std::uint64_t& position);
 
   private:
-    FlyweightShared* get_flyweight_shared(uint8_t key);
-
-    std::map<int, FlyweightShared*> m_flyweights_shared_pool;
+    std::shared_ptr<FlyweightShared> get_flyweight_shared(
+      const std::uint8_t& key);
+    
+    // Once a key is inserted, it's inheritly const, so don't specify.
+    // Once a value's linked to a key, it can change, so better not const.
+    std::map<std::uint8_t, std::shared_ptr<FlyweightShared>> 
+      m_flyweights_shared_pool;
 };
 
-#endif
+} // namespace Flyweight
+
+
+#endif // FLYWEIGHT_FACTORY_HPP
