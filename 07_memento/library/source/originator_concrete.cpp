13aa8369d809b774d885db43fcd8d0bc6c8a7ff6
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-03-07 10:52:32 +0000

    Draft of the improvements on the Memento pattern.

--- 07_memento/library/source/originator_concrete.cpp
+++ 07_memento/library/source/originator_concrete.cpp
@@ -1,26 +1,34 @@
 #include "07_memento/originator_concrete.hpp"
 #include "07_memento/memento_concrete.hpp"
 
+#include <iostream>
+#include <memory>
 
-OriginatorConcrete::OriginatorConcrete(int state)
+
+namespace Memento
+{
+    
+OriginatorConcrete::OriginatorConcrete(const std::int64_t& state)
 : m_state(state)
 {
     std::cout << "I'm the Originator. Just born." << std::endl;
 }
 
-Memento* OriginatorConcrete::create_memento()
+std::unique_ptr<MementoInterface> OriginatorConcrete::create_memento() const
 {
-    return new MementoConcrete(this, m_state);
+    return std::make_unique<MementoConcrete>(this, m_state);
 }
 
-void OriginatorConcrete::set_state(int state)
+void OriginatorConcrete::set_state(const std::int64_t& state)
 {
     std::cout << "I'm the Originator. Setting a new state." << std::endl;
 
     m_state = state;
 }
 
-void OriginatorConcrete::show_state()
+void OriginatorConcrete::show_state() const
 {
     std::cout << "I'm the Originator. My state is " << m_state << std::endl;
 }
+
+} // namespace Memento
