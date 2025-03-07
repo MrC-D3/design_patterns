ac4cba432127f831a4f0389e6520536f6245f94c
MrC-D3 <marco.filippi15@gmail.com>
2025-03-07 16:48:19 +0000

    Tentative solution.

--- 08_prototype/client_main.cpp
+++ 08_prototype/client_main.cpp
@@ -5,12 +5,16 @@ int main()
 {    
     PrototypeClient::PrototypeClient client;
 
-    auto prototypes = std::move( client.get_prototypes() );
-    auto copy1 = std::move( prototypes[0]->clone() );
-    auto copy2 = std::move( prototypes[1]->clone() );
+    auto prototypes = client.get_prototypes();
+    auto copy1 = std::move( (*prototypes)[0]->clone() );
+    auto copy2 = std::move( (*prototypes)[1]->clone() );
 
     copy1->to_string();
     copy2->to_string();
+
+    //
+    auto prototypes2 = client.get_prototypes();
+    (*prototypes2)[0]->to_string();
 
     return 0;
 }
