ef0a9e4940ffa8d841862680259d277f0e208345
MrC-D3 <marco.filippi15@gmail.com>
2025-03-11 17:50:49 +0000

    Draft of making Observer pattern more compliant to modern cpp.

--- 12_observer/library/include/12_observer/observer.hpp
+++ 12_observer/library/include/12_observer/observer.hpp
@@ -4,18 +4,27 @@
 #include "12_observer/observer_interface.hpp"
 #include "12_observer/subject.hpp"
 
+#include <memory>
 
-class Observer : public ObserverInterface
+
+namespace ObserverNS
+{
+  
+class Observer final : 
+  public ObserverInterface,
+  public std::enable_shared_from_this<Observer>
 {
   public:
-    Observer(Subject* subject);
+    Observer(const std::shared_ptr<Subject>& subject);
     ~Observer();
     
     void update() override;
 
   private:
-    Subject* m_subject;
-    int m_state;
+    std::shared_ptr<Subject> m_subject;
+    std::int64_t m_state;
 };
+
+} // namespace ObserverNS
 
 #endif
