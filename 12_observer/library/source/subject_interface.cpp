4307f7b12c8e2a20d44666b13291bd8ddcc940aa
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-03-11 19:02:26 +0000

    Final improvements on Observer pattern.

--- 12_observer/library/source/subject_interface.cpp
+++ 12_observer/library/source/subject_interface.cpp
@@ -1,5 +1,7 @@
 #include "12_observer/subject_interface.hpp"
 
+#include <algorithm> // For std::remove_if().
+
 
 namespace ObserverNS
 {
@@ -11,22 +13,35 @@ void SubjectInterface::attach(const std::shared_ptr<ObserverInterface>& observer
 
 void SubjectInterface::detach(const std::shared_ptr<ObserverInterface>& observer)
 {
-    for(auto it = m_observers.begin(); it != m_observers.end(); it++)
-    {
-        if(observer == *it)
-        {
-            m_observers.erase(it);
-            break;
-        }
-    }
+    // Cons of not using a loop: it goes over the whole container.
+    // Pros: 
+    //  - more readable, you know directly what's going to happen;
+    //  - more generic, you can change the condition of erase();
+    //  - delete any possible duplicates of observer. 
+    m_observers.erase(
+      std::remove_if(
+        m_observers.begin(), m_observers.end(), 
+        [&observer] (const std::shared_ptr<ObserverInterface>& o) 
+        { return o == observer; }
+      ),
+      m_observers.end()
+    );
+
+    // To stop at 1st match:
+    //  auto it = std::find(m_observers.begin(), m_observers.end(), observer);
+    //  if (it != m_observers.end()) m_observers.erase(it);
+    // In case of C++20:
+    //  std::erase_if(m_observers, [&observer](const auto& o) { return o == observer; });
+
 }
 
 void SubjectInterface::notify() const 
 {
-    for(auto it = m_observers.begin(); it != m_observers.end(); it++)
+    for (const auto& observer : m_observers)
     {
-        (*it)->update();
+        observer->update();
     }
 }
+
 
 } // namespace ObserverNS
