8f305c0024df627b920e026bcc5be36b5c5021cf
MrC-D3 <marco.filippi15@gmail.com>
2025-03-12 15:57:25 +0000

    Draft of mocking StateInterface.

--- 13_state/library/source/state_concrete.cpp
+++ 13_state/library/source/state_concrete.cpp
@@ -6,6 +6,11 @@
 namespace State
 {
 
+StateConcrete1::StateConcrete1(Context* context)
+  : StateInterface(context)
+{
+}
+
 void StateConcrete1::do_something()
 {
     std::cout << "I'm State1. This won't change the state." << std::endl;
@@ -15,12 +20,21 @@ void StateConcrete1::do_something_else()
 {
     std::cout << "I'm State1. This will change the state to State2." << 
       std::endl;
+
+    m_context->set_state(new StateConcrete2(m_context));
 }
 
+
+StateConcrete2::StateConcrete2(Context* context)
+  : StateInterface(context)
+{
+}
 
 void StateConcrete2::do_something()
 {
     std::cout << "I'm State2. This will change the state to State2." << std::endl;
+
+    m_context->set_state(new StateConcrete1(m_context));
 }
 
 void StateConcrete2::do_something_else()
