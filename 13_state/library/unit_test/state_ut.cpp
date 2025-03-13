5285160c2ac5b2eed4c6f59474433210d738857a
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-03-13 16:18:32 +0000

    Finish UT on State pattern.

--- 13_state/library/unit_test/state_ut.cpp
+++ 13_state/library/unit_test/state_ut.cpp
@@ -6,9 +6,14 @@
 
 
 using namespace State;
+using ::testing::AtLeast; 
 
 class State1Mock : public StateInterface
 {
+  public:
+    // Base class doesn't have a default c'tor, so Derived class must call it.
+    State1Mock() : StateInterface(nullptr) {};
+
     MOCK_METHOD(void, do_something, (), (override));
     MOCK_METHOD(void, do_something_else, (), (override));
 };
@@ -17,12 +22,16 @@ TEST(StateTestSuite, GeneralTest)
 {
     // Init.
     Context context;
-    State1Mock state1;
+    State1Mock* state1 = new State1Mock();
 
     // Prepare EXPECT_s.
-    EXPECT_CALL(state1, do_something)
+    EXPECT_CALL(*state1, do_something)
       .Times(AtLeast(1));
 
+    // Call.
     context.set_state(state1);
     context.do_something();
+
+    // Free (or warning from Gmock).
+    delete state1;
 }
