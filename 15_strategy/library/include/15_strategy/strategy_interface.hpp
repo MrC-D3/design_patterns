1981f0e2b41fa7ded96fcb5f566be62ec1ee0d60
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-03-20 07:57:24 +0000

    Some basic implementation of the Strategy pattern.

---
+++ 15_strategy/library/include/15_strategy/strategy_interface.hpp
@@ -0,0 +1,19 @@
+#ifndef STRATEGY_INTERFACE_HPP
+#define STRATEGY_INTERFACE_HPP
+
+
+namespace Strategy
+{
+
+class StrategyInterface
+{
+  public:
+    virtual ~StrategyInterface() = default;
+
+    virtual void do_something(std::int64_t data) = 0;
+};
+
+} // namespace Strategy
+
+
+#endif // STRATEGY_INTERFACE_HPP
