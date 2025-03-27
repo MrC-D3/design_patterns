70416a9d998a449185b6c5dc9c01d9974cf40a0d
MrC-D3 <marco.filippi15@gmail.com>
2025-03-27 19:00:24 +0000

    Go on with Composite pattern.

---
+++ 19_composite/library/include/19_composite/composite.hpp
@@ -0,0 +1,24 @@
+#ifndef COMPOSITE_HPP
+#define COMPOSITE_HPP
+
+#include "19_composite/component.hpp"
+#include <list>
+
+
+namespace CompositeNS
+{
+class Composite : public Component
+{
+  public:
+    void act() override;
+
+    void add(Component* child);
+
+  private:
+    std::list<Component*> m_children;
+};
+
+} // namespace CompositeNS
+
+
+#endif // COMPOSITE_HPP
