164873751ecd5a3a54ebb243b0ce615fcafac0fd
MrC-D3 <marco.filippi15@gmail.com>
2025-03-27 14:49:58 +0000

    Completed Abstract Factory library.

--- 18_abstract_factory/library/source/concrete_factory.cpp
+++ 18_abstract_factory/library/source/concrete_factory.cpp
@@ -1,1 +1,12 @@
 #include "18_abstract_factory/concrete_factory.hpp"
+
+
+namespace AbstractFactoryNS
+{
+
+ElementInterfaceA* ConcreteFactory::factoryElementA()
+{
+    return new ElementConcreteA1();
+}
+
+} // namespace AbstractFactoryNS
--- 18_abstract_factory/library/unit_test/abstract_factory_ut.cpp
+++ 18_abstract_factory/library/unit_test/abstract_factory_ut.cpp
@@ -3,31 +3,54 @@
 
 #include "18_abstract_factory/abstract_factory.hpp"
 #include "18_abstract_factory/element_interface.hpp"
+#include "18_abstract_factory/element_concrete.hpp"
 
 
-using namesapce AbstractFactoryNS;
+using namespace AbstractFactoryNS;
 using ::testing::AtLeast;
 
 // Mocks.
-
+class MockFactory : public AbstractFactory
+{
+  public:
+    ElementInterfaceA* factoryElementA() override
+    {
+        return new ElementConcreteA1();
+    }
+};
 
 // Fixtures.
 class AbstractFactoryFixture : public ::testing::Test
 {
   protected:
     AbstractFactoryFixture()
-    {}
+    {
+        m_oldBuffer = std::cout.rdbuf();
+        std::cout.rdbuf(m_buffer.rdbuf());
+    }
 
     ~AbstractFactoryFixture()
-    {}
+    {
+        std::cout.rdbuf(m_oldBuffer);
+    }
 
     void SetUp()
     {}
 
     void TearDown()
     {}
+
+    MockFactory m_factory;
+    std::streambuf* m_oldBuffer = nullptr;
+    std::stringstream m_buffer;
 };
 
 // Tests.
 TEST_F(AbstractFactoryFixture, Test1)
-{}
+{
+    auto elementA = m_factory.factoryElementA();
+        
+    elementA->doA();
+
+    EXPECT_NE(m_buffer.str().find("I'm ElementConcreteA1."), std::string::npos);
+}
