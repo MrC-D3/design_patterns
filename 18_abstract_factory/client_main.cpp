5391e0a59a178f583d5ce85723863c7104fc08e9
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-03-27 15:05:29 +0000

    Completed Abstract Factory example.

--- 18_abstract_factory/client_main.cpp
+++ 18_abstract_factory/client_main.cpp
@@ -1,4 +1,4 @@
-#include "18_abstract_factory/abstract_factory.hpp"
+#include "18_abstract_factory/concrete_factory.hpp"
 #include "18_abstract_factory/element_interface.hpp"
 
 
@@ -6,6 +6,11 @@ using namespace AbstractFactoryNS;
 
 int main()
 {
+    ConcreteFactory factory;
 
+    auto elementA = factory.factoryElementA();
+
+    elementA->doA();
+    
     return 0;
 }
