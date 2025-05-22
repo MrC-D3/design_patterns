e56b61a87b412333b90f0f4c4dc68999c29fbaf2
MrC-D3 <marco.filippi15@gmail.com>
2025-05-22 11:47:46 +0200

    Small changes on Bridge pattern example.

--- 01_bridge/implementation_hierarchy.hpp
+++ 01_bridge/implementation_hierarchy.hpp
@@ -8,18 +8,22 @@ class ColorInterface
 {
   public:
     virtual ~ColorInterface() noexcept = default;
+    // Default c'tors and operator= overloads (both copy and move).
 
     virtual void draw() const = 0;
 };
 
-class Color1 final : public ColorInterface
+class Blue final : public ColorInterface
 {
   public:
+    // Default d'tor, c'tors and operator= overloads (both copy and move).
+
     // Keyword "override" must be at the end.
     void draw() const override
     {
-        std::cout << "This is a shape drawn in Color1." << std::endl;
+        std::cout << "This is a shape drawn in Blue.\n";
     }
 };
+
 
 #endif // IMPLEMENTATION_HIERARCHY_HPP
