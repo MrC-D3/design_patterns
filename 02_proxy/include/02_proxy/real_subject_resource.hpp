e10c4f02ae2a2d1e327c2893be6797bd7301d89e
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-05-22 12:17:54 +0200

    Small changes on Proxy pattern example.

--- 02_proxy/include/02_proxy/real_subject_resource.hpp
+++ 02_proxy/include/02_proxy/real_subject_resource.hpp
@@ -9,23 +9,24 @@ class RealSubjectResource final : public SubjectResource
 {
   public:
     explicit RealSubjectResource(const std::string& url)
-      : SubjectResource(url)
+      : SubjectResource{url}
     {
         // RAII principle: Resource Acquisition Is Initialization.
-        std::cout << "I'm the RealSubject. I'm opening the Resource." << std::endl;
+        std::cout << "I'm the RealSubject. I'm opening the Resource.\n";
     }
 
     ~RealSubjectResource()
     {
-        std::cout << "I'm the RealSubject. I'm releasing the Resource." <<
-          std::endl;
+        std::cout << "I'm the RealSubject. I'm releasing the Resource.\n";
     }
+
+    // Default c'tors and operator= overloads, both copy and move.
 
     void access() override
     {
-        std::cout << "I'm the RealSubject. I'm accessing the Resource." <<
-          std::endl;
+        std::cout << "I'm the RealSubject. I'm accessing the Resource.\n";
     }
 };
+
 
 #endif
--- 02_proxy/include/02_proxy/subject_resource.hpp
+++ 02_proxy/include/02_proxy/subject_resource.hpp
@@ -11,17 +11,20 @@ class SubjectResource
     // A single-parameter-c'tor is a converting c'tor by default.
     // Use explicit to avoid it.
     explicit SubjectResource(const std::string& url)
-      : m_url(url)
+      : m_url{url}
     {}
 
     virtual ~SubjectResource() noexcept = default;
 
+    // Default c'tors and operator= overloads, both copy and move.
+
     void trivial_request() const
     {
         std::cout << "I'm the Subject. This request doesn't need to open the \
-          Resource." << std::endl;
+          Resource.\n";
     }
 
+    // Can't be const because it can modify data class in Proxy derived class.
     virtual void access() = 0;
 
   protected:
