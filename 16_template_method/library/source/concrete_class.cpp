a005cea056fc0148b56220b731d9033302f25d4c
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-05-26 22:28:04 +0200

    Small changes on Template Method pattern example.

--- 16_template_method/library/source/concrete_class.cpp
+++ 16_template_method/library/source/concrete_class.cpp
@@ -8,19 +8,17 @@ namespace TemplateMethod
 
 void ConcreteClass::primitive_operation1()
 {
-    std::cout << "I'm the ConcreteClass. I'm doing primitive operation 1."
-      << std::endl;
+    std::cout << "I'm the ConcreteClass. I'm doing primitive operation 1.\n";
 }
 
 void ConcreteClass::hook()
 {
-    std::cout << "I'm the ConcreteClass. I'm doing the hook." << std::endl;
+    std::cout << "I'm the ConcreteClass. I'm doing the hook.\n";
 }
 
 void ConcreteClass::primitive_operation2()
 {
-    std::cout << "I'm the ConcreteClass. I'm doing primitive operation 2." <<
-      std::endl;
+    std::cout << "I'm the ConcreteClass. I'm doing primitive operation 2.\n";
 }
 
 } // namespace TemplateMethod
