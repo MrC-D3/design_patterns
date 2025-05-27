584b217d627df36dfcd51552c5823172860eb455
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-05-27 16:29:33 +0200

    Small changes to the Abstract Factory pattern example.

--- 18_abstract_factory/library/include/18_abstract_factory/element_interface.hpp
+++ 18_abstract_factory/library/include/18_abstract_factory/element_interface.hpp
@@ -9,13 +9,7 @@ class ElementInterfaceA
 {
   public:
     virtual ~ElementInterfaceA() = default;
-    ElementInterfaceA() = default;
-
-    ElementInterfaceA(const ElementInterfaceA& origin) = default;
-    ElementInterfaceA& operator=(const ElementInterfaceA& origin) = default;
-
-    ElementInterfaceA(ElementInterfaceA&& origin) = default;
-    ElementInterfaceA& operator=(ElementInterfaceA&& origin) = default;
+    // Default c'tors and operator= overloads, both copy and move.
 
     virtual void doA() = 0;
 };
@@ -23,5 +17,6 @@ class ElementInterfaceA
 // Potentially ElementInterfaceB and other Product interfaces.
 
 } // namesapce AbstractFactoryNS
+
 
 #endif // ELEMENT_INTERFACE_HPP
