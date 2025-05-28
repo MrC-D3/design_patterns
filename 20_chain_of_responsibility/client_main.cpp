97accd6892146e60031ab64e737b041a87107072
MrC-D3 <marco.filippi15@gmail.com>
2025-05-28 07:01:13 +0000

    Significat changes to CoR pattern example.

--- 20_chain_of_responsibility/client_main.cpp
+++ 20_chain_of_responsibility/client_main.cpp
@@ -8,11 +8,21 @@ using namespace CoR;
 
 int main()
 {
-    auto handler0 = std::make_unique<HandlerConcrete>(nullptr, 0);
-    auto handler1 = std::make_unique<HandlerConcrete>(std::move(handler0), 1);
+    // Classic solution.
+    {
+        auto handler0 = std::make_unique<HandlerConcrete>(nullptr, 0);
+        auto handler1 = std::make_unique<HandlerConcrete>(std::move(handler0), 1);
 
-    handler1->handleRequest(1);
-    handler1->handleRequest(0);
+        handler1->handleRequest(1);
+        handler1->handleRequest(0);
+    }
+
+    // Alternative solution with C++11 std::function.
+    {
+        HandlerAlternative handler;
+        handler(0);
+        handler(1);
+    }
 
     return 0;
 }
--- 20_chain_of_responsibility/library/include/20_chain_of_responsibility/handler_interface.hpp
+++ 20_chain_of_responsibility/library/include/20_chain_of_responsibility/handler_interface.hpp
@@ -5,6 +5,7 @@
 
 #include <vector>
 #include <functional>
+#include <iostream>
 
 
 namespace CoR
@@ -36,8 +37,51 @@ class HandlerInterface
 */
 class HandlerAlternative
 {
+  public:
+    HandlerAlternative()
+    {
+        chain.push_back(
+          [](const std::int32_t request){
+            if(request == 0)
+            {
+              std::cout << "I'm Handler-0. Handling the request.\n";
+              return true;
+            }
+            else
+            {
+              return false;
+            }
+          }
+        );
+
+        chain.push_back(
+          [](const std::int32_t request){
+            if(request == 1)
+            {
+              std::cout << "I'm Handler-1. Handling the request.\n";
+              return true;
+            }
+            else
+            {
+              return false;
+            }
+          }
+        );
+    }
+
+    void operator()(const std::int32_t request)
+    {
+        for(auto& handler : chain)
+        {
+            if( handler(request) )
+            {
+                break;
+            }
+        }
+    }
+
   private:
-    std::vector< std::function<void(std::int32_t)> > chain;
+    std::vector< std::function<bool(const std::int32_t)> > chain;
 };
 
 } // namespace CoR
