47b1858021b2e8bf84840d0b8c877366eb5988dc
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-06-03 18:03:35 +0200

    One more update on Factory method.

--- 04_factory_method/client_main.cpp
+++ 04_factory_method/client_main.cpp
@@ -1,17 +1,21 @@
-// Alternative options of N ConcreteCreator-s:
-// 1) use template;
-// 2) use a parameter in createProduct() to distinguish between Product-s.
-// Design can start with Factory Method and then end up with Build or some other
-//  design pattern.
+// Another alternative implementation of the pattern that avoids having N 
+//  ConcreteCreator-s: use a parameter in createProduct() to distinguish between
+//  Product-s.
 
 #include "04_factory_method/concrete_creator.hpp"
+
+#include "04_factory_method/concrete_product.hpp"
 
 
 int main()
 {    
+    // Classic solution
     Factory::ConcreteCreator myCreator;
-
     myCreator.do_the_logic();
+
+    // Template solution (no v-table, so better performance).
+    Factory::CreatorT<Factory::ConcreteProduct> myCreatorT;
+    myCreatorT.do_the_logic();
 
     return 0;
 }
