865668a07f2e92b501d5b007eec26e372eb2115b
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-06-14 15:54:06 +0000

    Double Buffer pattern example.

---
+++ game_programming/00_double_buffer/library/include/00_double_buffer/double_buffer.hpp
@@ -0,0 +1,36 @@
+#ifndef DOUBLE_BUFFER_HPP
+#define DOUBLE_BUFFER_HPP
+
+#include <vector>
+#include <cstdint>
+#include <mutex>
+#include <condition_variable>
+
+
+namespace DoubleBufferNS
+{
+
+class DoubleBuffer
+{
+  public:
+    DoubleBuffer(std::uint32_t width, std::uint32_t height);
+    // Default d'tor, c'tors and operator= overloads, both copy and move.
+
+    void draw(std::uint16_t x, std::uint16_t y);
+    
+    void swap();
+
+    void clear();
+
+    const std::vector<std::uint32_t>& get_buffer();
+
+  private:
+    std::vector<std::uint32_t> m_current_buffer;
+    std::vector<std::uint32_t> m_next_buffer;
+    std::uint32_t m_height;
+    std::uint32_t m_width;
+};
+
+}
+
+#endif // DOUBLE_BUFFER_HPP
