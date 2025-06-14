89d969c17b6ebd7d7a0bf5ea60a36114d8aec90f
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-06-14 16:07:01 +0000

    Multi-thread test for Double Buffer pattern example.

--- game_programming/00_double_buffer/library/unit_test/double_buffer_ut.cpp
+++ game_programming/00_double_buffer/library/unit_test/double_buffer_ut.cpp
@@ -1,5 +1,7 @@
 #include "00_double_buffer/double_buffer.hpp"
 #include <thread>
+#include <mutex>
+#include <condition_variable>
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
@@ -84,48 +86,65 @@ TEST_F(DoubleBufferSuite, clear)
     }
 }
 
-//TEST_F(DoubleBufferSuite, DISABLED_sync)
-//{
-//    DoubleBuffer dBuffer(320, 200);
-//
-//    dBuffer.draw(0, 0);
-//    dBuffer.swap();
-//
-//    std::thread show(
-//      [&](){
-//        dBuffer.notify_consumed();
-//
-//        for(auto offset = 0; offset < 10; offset++)
-//        {
-//            auto buffer = dBuffer.get_buffer();
-//
-//            for(auto j = 0; j < 200; j++)
-//            {
-//                for(auto i = 0; i < 320; i++)
-//                {
-//                    if( (i==offset) && (j==0) )
-//                    {
-//                        ASSERT_EQ(buffer[j*320+i], 0xFFFFFFFF);
-//                    }
-//                    else
-//                    {
-//                        ASSERT_EQ(buffer[j*320+i], 0);
-//                    }
-//                }
-//            }
-//            
-//            dBuffer.notify_consumed();
-//            // 24 FPS: 1 frame ogni 41ms.
-//            //std::this_thread::sleep_for(41ms);
-//        }
-//    });
-//
-//    for(auto i = 0; i < 10; i++)
-//    {
-//        dBuffer.draw(i,0);
-//        dBuffer.swap();
-//        dBuffer.clear();
-//    }
-//
-//    show.join();
-//}
+TEST_F(DoubleBufferSuite, sync)
+{
+    DoubleBuffer dBuffer(320, 200);
+
+    std::mutex mutex_buffer;
+    std::condition_variable cv_buffer;
+    bool showable{false};
+
+    std::thread show(
+      [&](){
+        for(auto offset = 0; offset < 10; offset++)
+        {
+            std::unique_lock lock_buffer(mutex_buffer);
+            cv_buffer.wait(lock_buffer,
+              [&](){
+                return showable;
+            });
+
+            auto buffer = dBuffer.get_buffer();
+
+            showable = false;
+            lock_buffer.unlock();
+            cv_buffer.notify_all();
+
+            for(auto j = 0; j < 200; j++)
+            {
+                for(auto i = 0; i < 320; i++)
+                {
+                    if( (i==offset) && (j==0) )
+                    {
+                        ASSERT_EQ(buffer[j*320+i], 0xFFFFFFFF);
+                    }
+                    else
+                    {
+                        ASSERT_EQ(buffer[j*320+i], 0);
+                    }
+                }
+            }            
+            // 24 FPS: 1 frame ogni 41ms.
+            //std::this_thread::sleep_for(41ms);
+        }
+    });
+
+    for(auto i = 0; i < 10; i++)
+    {
+        dBuffer.draw(i,0);
+
+        std::unique_lock lock_buffer(mutex_buffer);
+        cv_buffer.wait(lock_buffer,
+          [&](){
+            return !showable;
+        });
+        dBuffer.swap();
+        showable = true;
+        lock_buffer.unlock();
+        cv_buffer.notify_all();
+
+        dBuffer.clear();
+    }
+
+    show.join();
+}
