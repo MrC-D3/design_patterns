e590190420d39806a47be3133ead994894854eaa
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-07-03 12:40:44 +0200

    New comments on the Factory Method pattern example.

--- gof/04_factory_method/client_main.cpp
+++ gof/04_factory_method/client_main.cpp
@@ -1,9 +1,10 @@
 // Another alternative implementation of the pattern that avoids having N 
 //  ConcreteCreator-s: use a parameter in createProduct() to distinguish between
 //  Product-s.
+// The Factory Method pattern allows those programs that know only at run-time 
+//  which objects they need, to be able to create them.
 
 #include "04_factory_method/concrete_creator.hpp"
-
 #include "04_factory_method/concrete_product.hpp"
 
 
