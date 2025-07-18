35b45740ef544edaf71d9b58ccdc24b04db955b1
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-07-18 18:14:06 +0200

    Hints on how to refactor code based on Singleton to make tests easier.

--- GoF/09_singleton/library/include/09_singleton/singleton.hpp
+++ GoF/09_singleton/library/include/09_singleton/singleton.hpp
@@ -1,8 +1,16 @@
+// The Singleton design pattern is considered an anti-pattern and one of the
+//  reasons is the difficulty to test it: a function under test that depends on 
+//  a Singleton may get that instance by itself (hidden hard-code dependency) 
+//  instead of receive it by parameter, so - if the case - refactor the code:
+//  - create a base class you can inherit from to make the Singleton class and
+//   the mock class used for testing;
+//  - make the function under test receives this base class as parameter, but
+//   this parameter is the Singleton instance by default.
+
 #ifndef SINGLETON_HPP
 #define SINGLETON_HPP
 
 #include <memory>
-#include <mutex>
 
 
 namespace SingletonNS
