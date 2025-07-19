cdd3cbd6eec1b0b192811252371936d34a162003
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-07-19 13:34:32 +0000

    Some comments for the Type Object pattern.

--- game_programming/05_type_object/library/include/05_type_object/type_object.hpp
+++ game_programming/05_type_object/library/include/05_type_object/type_object.hpp
@@ -1,3 +1,7 @@
+// Analogies with other design patterns:
+// - Flyweight, since both use an object to share data between other objects;
+// - State, since both delegate part of an object to another object.
+
 #ifndef TYPE_OBJECT_HPP
 #define TYPE_OBJECT_HPP
 
@@ -17,10 +21,14 @@ class TypeObject
     ~TypeObject() = default;
     TypeObject(const int id);
 
+    // Alternative: the client creates first the TypeObject then passes it to
+    //  the TypedObject's c'tor.
     TypedObject typedObjectFactory();
 
     // Not private just to avoid a getter method.
     int m_id;
+    // In game context, other data members can be: health, attack, etc.
+    // Also a pointer to a parent TypeObject to emulate inheritance.
 };
 
 /*
@@ -28,12 +36,21 @@ class TypeObject
 */
 class TypedObject
 {
+    // Make TypeObject a friend class so it can access the private c'tor.
+    // Friends are not affected by the access section (i.e. private or others).
+    friend class TypeObject;
+
   public:
     ~TypedObject() = default;
-    TypedObject(TypeObject& type);
     // Default c'tor and operator= overloads, both copy and move.
 
   private:
+    // Private so a TypedObject can be created only by the factory method.
+    TypedObject(TypeObject& type);
+
+    // If m_type can change, you must ensure that any TypedObject data member is
+    //  consistent with the new m_type; e.g. a Monster whose new type is Troll
+    //  so its new health can't be over a Troll's maximum health.
     TypeObject& m_type;
 };
 
