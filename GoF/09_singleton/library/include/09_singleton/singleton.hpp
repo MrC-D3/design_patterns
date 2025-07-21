7b2807e57f4a1aaa05da07e36af2c3f5aff33c3c
MrC-D3 <27030240+MrC-D3@users.noreply.github.com>
2025-07-21 16:46:30 +0200

    How to refactor the Singleton example to be a better pattern.

--- GoF/09_singleton/library/include/09_singleton/singleton.hpp
+++ GoF/09_singleton/library/include/09_singleton/singleton.hpp
@@ -1,12 +1,3 @@
-// The Singleton design pattern is considered an anti-pattern and one of the
-//  reasons is the difficulty to test it: a function under test that depends on 
-//  a Singleton may get that instance by itself (hidden hard-code dependency) 
-//  instead of receive it by parameter, so - if the case - refactor the code:
-//  - create a base class you can inherit from to make the Singleton class and
-//   the mock class used for testing;
-//  - make the function under test receives this base class as parameter, but
-//   this parameter is the Singleton instance by default.
-
 #ifndef SINGLETON_HPP
 #define SINGLETON_HPP
 
@@ -36,6 +27,69 @@ class Singleton
 
     std::int64_t m_counter;
 };
+
+// The Singleton design pattern is considered an anti-pattern and one of the
+//  reasons is the difficulty to test it: a function under test that depends on 
+//  a Singleton may get that instance by itself (hidden hard-code dependency) 
+//  instead of receive it by parameter, so - if the case - refactor the code:
+//  - create a class C with all the Singleton's methods as virtual, but without
+//   the single static instance;
+//  - create a struct S that just wraps a single instance I of the class C.
+//  Now you can:
+//  - mock class C for testing purpose;
+//  - pass S::I as default parameter to all the functions that was depending on
+//   the Singleton.
+class ClassC
+{
+  public:
+    // Virtual so it can be used as base class for mocking.
+    virtual ~ClassC() = default;
+    explicit ClassC(const std::int64_t counter = 0);
+    // Default c'tors and operator= overloads, both copy and move.
+
+    virtual void print_counter() const;
+
+  protected:
+    std::int64_t m_counter;
+};
+
+struct SingletonWrapper
+{
+    ClassC instance;
+};
+
+ClassC& getDefaultClassC()
+{
+    // Lazy initialization.
+    static SingletonWrapper wrapper;
+    return wrapper.instance;
+}
+
+// Usually Singletons run in groups, so the implementation change a little.
+class Service1
+{};
+class Service2
+{};
+
+struct Services
+{
+    Services(Service1& instanceS1_, Service2& instanceS2_)
+    : instanceS1{instanceS1_}, instanceS2{instanceS2_}
+    {}
+    
+    // By references, so they must be stored somewhere else.
+    Service1& instanceS1;
+    Service2& instanceS2;
+};
+
+Services& getDefaultServices()
+{
+    static Service1 serviceS1;
+    static Service2 serviceS2;
+    static Services services(serviceS1, serviceS2);
+
+    return services;
+}
 
 } // namespace SingletonNS
 
